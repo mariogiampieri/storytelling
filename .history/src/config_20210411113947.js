3d1c703803d8a99d8f4939fd72146f69ed652559
marioag <mariogiampieri@gmail.com>
2021-04-12 08:51:36 -0400

    initial commit

---
+++ .history/src/config_20210411113947.js
@@ -0,0 +1,75 @@
+var locations = {
+    'alan-home': {
+        center: [-104.94775, 39.97077],
+        zoom: 16.58,
+        pitch: 8.00,
+        bearing: 0.00
+    },
+    'horizon': {
+        // horizon
+        center: [-104.92620, 39.94445],
+        zoom: 16.43,
+        pitch: 38.50,
+        bearing: 0.00
+    }
+}
+
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoibWFyaW9hZyIsImEiOiJja25kYnZ6bnExOGNxMm5uczAyeGkzbm9jIn0.9qgZHh97osM101AWsG7Jgw',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    theme: 'light',
+    use3dTerrain: false,
+    title: 'The Title Text of this Story',
+    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By a Digital Storyteller',
+    footer: 'Source: source citations, etc.',
+
+
+    chapters: [{
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Display Title',
+            image: './path/to/image/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: ,
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'other-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
---
+++ .history/src/config_20210412081746.js
@@ -0,0 +1,347 @@
+var locations = {
+    'alan-home': {
+        center: [-104.94775, 39.97077],
+        zoom: 16.58,
+        pitch: 8.00,
+        bearing: 0.00
+    },
+    'horizon': {
+        center: [-104.92620, 39.94445],
+        zoom: 16.43,
+        pitch: 38.50,
+        bearing: 0.00
+    },
+    'baskin-robin': {
+        center: [-104.94072, 39.91237],
+        zoom: 16.98,
+        pitch: 20.00,
+        bearing: -0.80
+    },
+    'maker-faire': {
+        center: [-73.83637, 40.74775],
+        zoom: 15.74,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'jackson-heights': {
+        center: [-73.89149, 40.74741],
+        zoom: 16.55,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'helens': {
+        center: [-73.92872, 40.76054],
+        zoom: 16.51,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'times-square': {
+        center: [-73.98568, 40.75734],
+        zoom: 16.10,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'mckibben': {
+        center: [-73.93716, 40.70573],
+        zoom: 17.15,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'pour-george': {
+        center: [-73.99843, 40.73321],
+        zoom: 16.49,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'insomnia': {
+        center: [-73.99891, 40.73322],
+        zoom: 16.49,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'otto': {
+        center: [-73.99608, 40.73197],
+        zoom: 16.00,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'cornelia': {
+        center: [-73.91622, 40.68967],
+        zoom: 16.00,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'rockaway': {
+        center: [-73.81522, 40.58298],
+        zoom: 15.89,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'snug-harbor': {
+        center: [-74.10314, 40.64243],
+        zoom: 15.94,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'prospect-park-lake': {
+        center: [-73.97200, 40.65536],
+        zoom: 15.79,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'alans-suydam': {
+        center: [-73.91910, 40.70625],
+        zoom: 16.80,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'troutman': {
+        center: [-73.92977, 40.69969],
+        zoom: 16.00,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'outside-castlebraid': {
+        center: [-73.92941, 40.69980],
+        zoom: 17.51,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'harriman': {
+        center: [-74.11355, 41.23440],
+        zoom: 15.24,
+        pitch: 36.00,
+        bearing: -22.41
+    },
+    'philly': {
+        center: [-75.16191, 39.94329],
+        zoom: 16.07,
+        pitch: 15.50,
+        bearing: 19.99
+    },
+    'allentown': {
+        center: [-75.51597, 40.60949],
+        zoom: 12.20,
+        pitch: 15.50,
+        bearing: 0.00
+    },
+    'luxor': {
+        center: [-115.17543, 36.09529],
+        zoom: 16.99,
+        pitch: 43.50,
+        bearing: -10.40
+    },
+    'coors-lady': {
+        center: [-115.17271, 36.10238],
+        zoom: 17.07,
+        pitch: 43.50,
+        bearing: -10.40
+    },
+    'reno': {
+        center: [-119.81177, 39.52726],
+        zoom: 17.77,
+        pitch: 25.50,
+        bearing: -15.96
+    },
+    'bunny-ranch': {
+        center: [-119.67170, 39.21736],
+        zoom: 16.00,
+        pitch: 5.00,
+        bearing: 0.00
+    },
+    'venice': {
+        center: [-118.47709, 33.99030],
+        zoom: 14.92,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'orange-county': {
+        center: [-118.01983, 33.83867],
+        zoom: 13.87,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'alcatraz': {
+        center: [-122.42232, 37.82630],
+        zoom: 15.71,
+        pitch: 4.50,
+        bearing: 7.23
+    },
+    'tenderloin': {
+        center: [-122.41506, 37.78364],
+        zoom: 16.32,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'sf-piers': {
+        center: [-122.39568, 37.79759],
+        zoom: 17.13,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'sacramento': {
+        center: [-121.49408, 38.56829],
+        zoom: 14.85,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'carmel': {
+        center: [-121.91477, 36.55345],
+        zoom: 14.11,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'slo': {
+        center: [-120.66242, 35.27815],
+        zoom: 13.13,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'hwy-1': {
+        center: [-121.37839, 35.82286],
+        zoom: 12.86,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'central-square': {
+        center: [-71.10165, 42.36402],
+        zoom: 16.22,
+        pitch: 45.00,
+        bearing: -40.00
+    },
+    'worcester': {
+        center: [-71.80048, 42.26674],
+        zoom: 16.81,
+        pitch: 23.00,
+        bearing: 1.74
+    },
+    'terminal-5': {
+        center: [-73.99286, 40.76954],
+        zoom: 16.00,
+        pitch: 23.00,
+        bearing: 1.74
+    },
+    'marks-roof': {
+        center: [-73.98341, 40.72586],
+        zoom: 17.15,
+        pitch: 8.50,
+        bearing: 17.74
+    },
+    'webster-hall': {
+        center: [-73.98921, 40.73174],
+        zoom: 17.21,
+        pitch: 8.50,
+        bearing: 17.74
+    },
+    'bk-bazaar-2': {
+        center: [-73.95476, 40.72997],
+        zoom: 17.69,
+        pitch: 21.00,
+        bearing: -7.78
+    },
+    'ioby-bk-brew': {
+        center: [-73.95733, 40.72141],
+        zoom: 17.34,
+        pitch: 21.00,
+        bearing: -7.78
+    },
+    'bk-bazaar': {
+        center: [-73.95504, 40.72447],
+        zoom: 16.00,
+        pitch: 21.00,
+        bearing: -7.78
+    },
+    'ogallala-beach': {
+        center: [-101.72344, 41.20460],
+        zoom: 14.94,
+        pitch: 0.00,
+        bearing: -0.16
+    },
+    'ogallala-movies': {
+        center: [-101.71935, 41.12597],
+        zoom: 16.00,
+        pitch: 0.00,
+        bearing: -0.16
+    },
+    'kevins': {
+        center: [-105.02320, 39.84536],
+        zoom: 17.08,
+        pitch: 0.00,
+        bearing: -0.16
+    },
+    'eudora': {
+        center: [-104.92973, 39.90520],
+        zoom: 16.00,
+        pitch: 45.00,
+        bearing: -40.00
+    },
+    '130th': {
+        center: [-104.89334, 39.93371],
+        zoom: 17.67,
+        pitch: 48.50,
+        bearing: 15.36
+    }
+
+
+
+
+}
+
+
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoibWFyaW9hZyIsImEiOiJja25kYnZ6bnExOGNxMm5uczAyeGkzbm9jIn0.9qgZHh97osM101AWsG7Jgw',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    theme: 'light',
+    use3dTerrain: false,
+    title: 'The Title Text of this Story',
+    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By a Digital Storyteller',
+    footer: 'Source: source citations, etc.',
+
+
+    chapters: [{
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Display Title',
+            image: './img/alans-house.jpeg',
+            description: '',
+            location: locations['alan-home'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'other-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: './img/pirates-pirate.jpeg',
+            description: 'Copy these sections to add to your story.',
+            location: locations['horizon'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'other-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: '3 Title',
+            image: './img/br-puke.jpeg',
+            description: 'Copy these sections to add to your story.',
+            location: locations['baskin-robbin'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
