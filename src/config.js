7d2e57198a07bbebd6828127f6a6330d3fe8de37
mariogiampieri <mariogiampieri@gmail.com>
2021-04-13 07:54:15 -0400

    add config

---
+++ src/config.js
@@ -0,0 +1,581 @@
+var locations = {
+    'alan-home': {
+        center: [-104.94775, 39.97077],
+        zoom: 16.58,
+        pitch: 8.00,
+        bearing: 0.00
+    },
+    'horizon': {
+        center: [-104.92620, 39.94445],
+        zoom: 16.43,
+        pitch: 38.50,
+        bearing: 0.00
+    },
+    'baskin-robin': {
+        center: [-104.94072, 39.91237],
+        zoom: 16.98,
+        pitch: 20.00,
+        bearing: -0.80
+    },
+    'ogallala-walmart': {
+        center: [-103.23631, 40.62620],
+        zoom: 15.32,
+        pitch: 20.00,
+        bearing: -0.80
+    },
+    'maker-faire': {
+        center: [-73.83637, 40.74775],
+        zoom: 15.74,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'jackson-heights': {
+        center: [-73.89149, 40.74741],
+        zoom: 16.55,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'helens': {
+        center: [-73.92872, 40.76054],
+        zoom: 16.51,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'times-square': {
+        center: [-73.98568, 40.75734],
+        zoom: 16.10,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'mckibben': {
+        center: [-73.93716, 40.70573],
+        zoom: 17.15,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'pour-george': {
+        center: [-73.99843, 40.73321],
+        zoom: 16.49,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'insomnia': {
+        center: [-73.99891, 40.73322],
+        zoom: 16.49,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'otto': {
+        center: [-73.99608, 40.73197],
+        zoom: 16.00,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'cornelia': {
+        center: [-73.91622, 40.68967],
+        zoom: 16.00,
+        pitch: 6.50,
+        bearing: -11.59
+    },
+    'rockaway': {
+        center: [-73.81522, 40.58298],
+        zoom: 15.89,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'snug-harbor': {
+        center: [-74.10314, 40.64243],
+        zoom: 15.94,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'prospect-park-lake': {
+        center: [-73.97200, 40.65536],
+        zoom: 15.79,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'alans-suydam': {
+        center: [-73.91910, 40.70625],
+        zoom: 16.80,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'troutman': {
+        center: [-73.92977, 40.69969],
+        zoom: 16.00,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'outside-castlebraid': {
+        center: [-73.92941, 40.69980],
+        zoom: 17.51,
+        pitch: 2.00,
+        bearing: 10.67
+    },
+    'harriman': {
+        center: [-74.11355, 41.23440],
+        zoom: 15.24,
+        pitch: 36.00,
+        bearing: -22.41
+    },
+    'philly': {
+        center: [-75.16191, 39.94329],
+        zoom: 16.07,
+        pitch: 15.50,
+        bearing: 19.99
+    },
+    'allentown': {
+        center: [-75.51597, 40.60949],
+        zoom: 12.20,
+        pitch: 15.50,
+        bearing: 0.00
+    },
+    'luxor': {
+        center: [-115.17543, 36.09529],
+        zoom: 16.99,
+        pitch: 43.50,
+        bearing: -10.40
+    },
+    'coors-lady': {
+        center: [-115.17271, 36.10238],
+        zoom: 17.07,
+        pitch: 43.50,
+        bearing: -10.40
+    },
+    'reno': {
+        center: [-119.81177, 39.52726],
+        zoom: 17.77,
+        pitch: 25.50,
+        bearing: -15.96
+    },
+    'bunny-ranch': {
+        center: [-119.67170, 39.21736],
+        zoom: 16.00,
+        pitch: 5.00,
+        bearing: 0.00
+    },
+    'venice': {
+        center: [-118.47709, 33.99030],
+        zoom: 14.92,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'orange-county': {
+        center: [-118.01983, 33.83867],
+        zoom: 13.87,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'alcatraz': {
+        center: [-122.42232, 37.82630],
+        zoom: 15.71,
+        pitch: 4.50,
+        bearing: 7.23
+    },
+    'tenderloin': {
+        center: [-122.41506, 37.78364],
+        zoom: 16.32,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'sf-piers': {
+        center: [-122.39568, 37.79759],
+        zoom: 17.13,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'oracle-park': {
+        center: [-122.38921, 37.77845],
+        zoom: 17.06,
+        pitch: 20.00,
+        bearing: -0.80
+    },
+    'sacramento': {
+        center: [-121.49408, 38.56829],
+        zoom: 14.85,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'carmel': {
+        center: [-121.91477, 36.55345],
+        zoom: 14.11,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'slo': {
+        center: [-120.66242, 35.27815],
+        zoom: 13.13,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'hwy-1': {
+        center: [-121.37839, 35.82286],
+        zoom: 12.86,
+        pitch: 45.00,
+        bearing: 0.00
+    },
+    'santa-barbara': {
+        center: [-119.69133, 34.41353],
+        zoom: 13.90,
+        pitch: 20.00,
+        bearing: -0.80
+    },
+    'santa-monica': {
+        center: [-118.49979, 34.00764],
+        zoom: 15.46,
+        pitch: 20.00,
+        bearing: -0.80
+    },
+    'central-square': {
+        center: [-71.10165, 42.36402],
+        zoom: 16.22,
+        pitch: 45.00,
+        bearing: -40.00
+    },
+    'worcester': {
+        center: [-71.80048, 42.26674],
+        zoom: 16.81,
+        pitch: 23.00,
+        bearing: 1.74
+    },
+    'terminal-5': {
+        center: [-73.99286, 40.76954],
+        zoom: 16.00,
+        pitch: 23.00,
+        bearing: 1.74
+    },
+    'marks-roof': {
+        center: [-73.98341, 40.72586],
+        zoom: 17.15,
+        pitch: 8.50,
+        bearing: 17.74
+    },
+    'webster-hall': {
+        center: [-73.98921, 40.73174],
+        zoom: 17.21,
+        pitch: 8.50,
+        bearing: 17.74
+    },
+    'bk-bazaar-2': {
+        center: [-73.95476, 40.72997],
+        zoom: 17.69,
+        pitch: 21.00,
+        bearing: -7.78
+    },
+    'ioby-bk-brew': {
+        center: [-73.95733, 40.72141],
+        zoom: 17.34,
+        pitch: 21.00,
+        bearing: -7.78
+    },
+    'bk-bazaar': {
+        center: [-73.95504, 40.72447],
+        zoom: 16.00,
+        pitch: 21.00,
+        bearing: -7.78
+    },
+    'ogallala-beach': {
+        center: [-101.72344, 41.20460],
+        zoom: 14.94,
+        pitch: 0.00,
+        bearing: -0.16
+    },
+    'ogallala-movies': {
+        center: [-101.71935, 41.12597],
+        zoom: 16.00,
+        pitch: 0.00,
+        bearing: -0.16
+    },
+    'kevins': {
+        center: [-105.02320, 39.84536],
+        zoom: 17.08,
+        pitch: 0.00,
+        bearing: -0.16
+    },
+    'eudora': {
+        center: [-104.92973, 39.90520],
+        zoom: 16.00,
+        pitch: 45.00,
+        bearing: -40.00
+    },
+    '130th': {
+        center: [-104.89334, 39.93371],
+        zoom: 17.67,
+        pitch: 48.50,
+        bearing: 15.36
+    }
+
+
+
+
+}
+
+
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoibWFyaW9hZyIsImEiOiJja25kYnZ6bnExOGNxMm5uczAyeGkzbm9jIn0.9qgZHh97osM101AWsG7Jgw',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    theme: 'light',
+    use3dTerrain: false,
+    title: 'You are Alan',
+    // subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    // byline: 'By a Digital Storyteller',
+    footer: 'Bad Friends bb',
+
+
+    chapters: [{
+            id: 'alans-house',
+            alignment: 'left',
+            hidden: false,
+            title: 'A Fashion Icon',
+            image: './img/alans-house.jpeg',
+            description: `We used to hang out in your basement and watch each other pee`,
+            location: locations['alan-home'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pirates-pirate',
+            alignment: 'right',
+            hidden: false,
+            title: `A Pirate's Pirate`,
+            image: './img/pirates-pirate.jpeg',
+            description: 'I think we probably had too much fun at school not doing school things.',
+            location: locations['horizon'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'baskin-robins',
+            alignment: 'right',
+            hidden: false,
+            title: 'Jackass',
+            image: './img/br-puke.jpeg',
+            description: `Some things haven't change- you'll still do a lot for $20 ;)`,
+            location: locations['baskin-robin'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'ogallala-walmart',
+            alignment: 'left',
+            hidden: false,
+            title: 'Riding in cars with bois',
+            image: './img/ogallala-walmart.jpeg',
+            description: 'We kinda had a King of the Hill thing going on in Walmart parking lots... we spent a LOT of time at Walmart.',
+            location: locations['ogallala-walmart'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'ogallala-beach',
+            alignment: 'right',
+            hidden: false,
+            title: 'Babewatch',
+            image: './img/ogallala-day.jpeg',
+            description: 'Remember when you almost drowned in that lake? Me too!',
+            location: locations['ogallala-beach'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'ogallala-movies',
+            alignment: 'left',
+            hidden: false,
+            title: 'Date night',
+            image: '',
+            description: 'Then we went to the movies, and got kicked out. and then we got blts across the street, and got kicked out again.',
+            location: locations['ogallala-movies'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'reno',
+            alignment: 'left',
+            hidden: false,
+            title: 'Biggest Little City on that trip',
+            image: './img/reno.jpeg',
+            description: `I did not like Reno. It started my (so far) life-long distaste for casinos. Scary vibes man`,
+            location: locations['reno'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'bunny-ranch',
+            alignment: 'left',
+            hidden: false,
+            title: 'Bunny Ranch',
+            image: '',
+            description: '... and then things got even more scary when we drove for like an hour to this weird place, took one look, and dipped.',
+            location: locations['bunny-ranch'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'alan-sleeping',
+            alignment: 'left',
+            hidden: false,
+            title: `Goin' to California`,
+            image: './img/ca-road.jpeg',
+            description: 'This picture was definitely taken somewhere else, but it was pretty par for the course for those long drives through the desert. Also, Sac-town was very sleepy.',
+            location: locations['sacramento'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'sf-piers',
+            alignment: 'left',
+            hidden: false,
+            title: '21+',
+            image: './img/san-fran.jpeg',
+            description: 'This is when we learned that you kinda have to be 21 to do most things. Having said that, we had a good time being cool kids around San Fran',
+            location: locations['sf-piers'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'alcatraz',
+            alignment: 'left',
+            hidden: false,
+            title: `Pics or it didn't happen`,
+            image: './img/alcatraz.jpeg',
+            description: `I don't remember anything about Alcatraz, but this picture proved that we were there.`,
+            location: locations['alcatraz'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'giants',
+            alignment: 'left',
+            hidden: false,
+            title: 'A nice view, and also baseball. Some great people watching, and it was just beautiful.',
+            image: './img/gg-park.jpeg',
+            description: '',
+            location: locations['oracle-park'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'hwy-1',
+            alignment: 'left',
+            hidden: false,
+            title: 'Open Wide',
+            image: './img/hwy-1.jpeg',
+            description: 'I remember the moment that we finally broke through those amazing hills that felt like they went on forever, and then to just see the entire ocean just open up. That was such an amazing moment!',
+            location: locations['hwy-1'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'santa-barbara',
+            alignment: 'left',
+            hidden: false,
+            title: `Tag You're It`,
+            image: './img/santa-barbara.jpeg',
+            description: `We ate well, we saw that Banksy movie, and then painted the town red.`,
+            location: locations['santa-barbara'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'santa-monica',
+            alignment: 'left',
+            hidden: false,
+            title: 'Car Park Fashion Show',
+            image: './img/la-crabs-3.jpeg',
+            description: 'We lived out of that trunk, and it showed! It really showed.. but it was awesome and somehow we pulled it off...',
+            location: locations['santa-monica'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'santa-monica-2',
+            alignment: 'left',
+            hidden: false,
+            title: '',
+            image: './img/la-crabs.jpeg',
+            description: '... well, you pulled it off. I still have that shirt.',
+            location: locations['santa-monica'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'vegas-1',
+            alignment: 'left',
+            hidden: false,
+            title: 'Temple of Coors Lite',
+            image: './img/luxor.jpeg',
+            description: `I just remember shot-gunning beers in that ridiculous bathroom and then hitting the town. Still weird to be there and not be 21 but you did find Tina so that was a win`,
+            location: locations['luxor'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'eudora',
+            alignment: 'left',
+            hidden: false,
+            title: 'Bassment',
+            image: './img/destroy-basement.jpeg',
+            description: `I really love how we spent most of that summer in a dark basement, either mine or Kevin's. Making shirts together was so much fun, I wouldn't trade that summer for anything man.`,
+            location: locations['eudora'],
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
